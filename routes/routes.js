b66a84c738ec6cc811841b8f44e811f5e214062d
andrepaixaao <40185649+andrepaixaao@users.noreply.github.com>
2019-11-17 17:44:19 +0000

    Add files via upload

--- routes/routes.js
+++ routes/routes.js
@@ -12,15 +12,4 @@ router.get('/', function (req, res, next) {
 
 
 
-router.get('/', function(req, res, next) {
-    SmartShopDAO.getItems(function(err,result){
-        if (err) {
-            // sending error because its for learning/debugging
-            // real project would only send a general message
-            res.status(result.code).json(err);
-            return;
-        }
-        res.status(result.code).send(result.data);
-    },next)
-});
 module.exports = router;
