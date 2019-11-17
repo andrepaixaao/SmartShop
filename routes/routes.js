46a5b83685c35742555fcb927e62486a6b38456f
andrepaixaao <40185649+andrepaixaao@users.noreply.github.com>
2019-11-17 17:37:57 +0000

    Add files via upload

--- routes/routes.js
+++ routes/routes.js
@@ -7,7 +7,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('./index.html', { title: 'Index' });
+  res.render('index', { title: 'Index' });
 });
 
 
