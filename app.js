10b47a98121979a3a9a75cf6115a42fd02aae3a4
andrepaixaao <40185649+andrepaixaao@users.noreply.github.com>
2019-11-17 22:09:02 +0000

    Add files via upload

--- app.js
+++ app.js
@@ -5,13 +5,12 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname,'views'));
+app.set('view engine','hbs');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -20,22 +19,16 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/login', indexRouter);
+app.use('/carrinho', indexRouter);
+app.use('/perfil', indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,15 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Index' });
 });
 
+router.get('/carrinho', function(req, res, next) {
+  res.render('carrinho', { title: 'Carrinho' });
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('indexLogin', { title: 'SmartShop' });
+});
+
+router.get('/perfil', function(req, res, next) {
+  res.render('perfil', { title: 'SmartShop' });
+});
 module.exports = router;
